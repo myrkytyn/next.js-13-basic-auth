c7f45511e349eada6ab993af5f8283afb49c21bd
Andrii Yusyp <a.yusyp@apiko.com>
2023-07-29 23:02:56 +0300

    stub

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,30 @@
-import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
+// import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-const options = { users: [{ name: "test", password: "test" }] };
-export const middleware = createNextAuthMiddleware(options);
+// const options = { users: [{ name: "test1", password: "test1" }] };
+// export const middleware = createNextAuthMiddleware(options);
 
-export const config = {
-  matcher: ["/(.*)"],
-};
-
-
+// export const config = {
+//   matcher: ["/(.*)"],
+// };
 
 // Uncomment this part to use second approach
 
-/*
 import { NextResponse } from 'next/server';
-import { nextBasicAuthMiddleware } from 'nextjs-basic-auth-middleware'
+import { nextBasicAuthMiddleware } from 'nextjs-basic-auth-middleware';
 
-const options = {users: [{ name: "test", password: "test" }]};
+const options = { users: [{ name: 'test2', password: 'test2' }] };
 export const middleware = (req: any) => {
-    nextBasicAuthMiddleware(options, req)
-    return NextResponse.next()
-}
+  const NextResponse = nextBasicAuthMiddleware(options, req);
+
+  // if (NextResponse.status === 401) {
+  //   return NextResponse;
+  // }
+  return NextResponse;
+
+  // Your other middleware functions here
+  // return NextResponse.next();
+};
 
 export const config = {
-    matcher: ['/(.*)'],
-}
-*/
+  matcher: ['/(.*)'],
+};
